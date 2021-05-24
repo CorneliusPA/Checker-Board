8000f89e816814321332ece4c899580922dd744d
Cornelius Allison <75234559+CorneliusPA@users.noreply.github.com>
2021-05-24 15:50:38 -0700

    Checker Board First Draft 5/24/21
    Checker Board Set.
    Checker Pieces Set.
    Next Objectives:
    Condense Code
    Apply JavaScript
    Replace Checker Pieces with images via a tags.

---
+++ script.js
@@ -0,0 +1,20 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+  
+  function clearBorder(){
+var x = document.getElementById("ArtGrid");
+
+x.style.border="0px solid black";
+ }
